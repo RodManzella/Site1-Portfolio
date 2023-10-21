b88a8474c244fec4f69f88b2012418a26e3a5320
RodManzella <manzellateixeira@gmail.com>
2023-10-21 20:13:10 -0300

    commit

---
+++ js/game.js
@@ -0,0 +1,16 @@
+
+const start_button = document.querySelector("#play-button")
+const phrase = document.querySelector(".game-info")
+
+start_button.onclick = () =>{
+    phrase.innerHTML = ""
+    start_button.style.display = "none"
+    showScreen()
+
+}
+
+function showScreen(){
+
+}
+
+
