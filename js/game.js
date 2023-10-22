8403996e2fa3b966929d3ed4bcbe195648b93f20
RodManzella <manzellateixeira@gmail.com>
2023-10-22 13:37:21 -0300

    commit

--- js/game.js
+++ js/game.js
@@ -7,38 +7,71 @@ let bulls_text = document.querySelector("#bulls")
 let cows_text = document.querySelector("#cows")
 let valorInput;
 let rounds = 10
+let randomNumber = random_number()
+let cows;
+let bulls;
 
 
 
 start_button.onclick = () =>{
-    start_button_screen.style.display = "none"
-    showScreen()
+    start_button_screen.style.display = "none";
+    showScreen();
 }
 
+guessButton.onclick = () => {
+    let guess = document.getElementById("guess-input").value;
+     let result = check_guess(randomNumber, guess);
 
+     console.log("teste 0.5")
+     console.log(randomNumber)
+     cows = 0;
+     bulls = 0;
+     if(rounds == 0) defeatScreen();
+     console.log("teste 1")
 
+     if(result){
+         victoryScreen();
+         console.log("teste 2")
+     }else{
+        console.log("teste 3")
+        console.log(typeof(guess))
+          rounds--;
+          update_bulls_cows(randomNumber, guess);
+          document.getElementById("guess-input").value =  "";
+     }
+  }
 
-
-
-
-function guessNumber(){
-    return document.getElementById("#guess-input")
-}
 
 function random_number(){
     return Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
 }
 
-
 function showScreen(){
-    game_screen.style.display = "block"
+    game_screen.style.display = "block";
 }
 
  function game_start(){
-     document.querySelector(".lifes").innerHTML = rounds
+     document.querySelector(".lifes").innerHTML = rounds;
 
-     
+}
 
+function update_bulls_cows(realNumber, guess){
+    for(let i = 0; i < 4; i++){
+        if(guess[i] == realNumber[i]){
+            bulls++;
+        }
+
+        if(realNumber.indexOf(guess[i]) !== -1){
+            cows++;
+        }
+    }
+}
+
+function check_guess(realNumber, guess){
+    if(realNumber ==  guess){
+        return true;
+    }
+    return false;
 }
 
 
